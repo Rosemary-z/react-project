e643915a4ac0137cb3135f8263aa75f0e24b1937
Rosemary-z <1247562661@qq.com>
2021-01-11 11:50:21 +0800

    '上传岗位分类'

--- src/api/job.js
+++ src/api/job.js
@@ -1,8 +1,8 @@
 import request from './request.js';
 // 录入岗位分类
-// export const enterPC = (data) => { 
-//     return request.post('/1.1/classes/positionList',data)
-// }
+export const enterPC = (data) => {
+    return request.post('/1.1/classes/positionCategory', data)
+}
 // 查询职位列表
 export const getPositionDetails = () => {
     return request.get('/1.1/classes/positionCategory ')
